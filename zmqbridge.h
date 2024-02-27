f7b856551c3c7dd90ab0945a9027538bafb4d65c
JSON <jasonrlester@yahoo.com>
2024-02-27 14:53:37 -0800

    Update zmqbridge.h
    Windows still not tested

--- zmqbridge.h
+++ zmqbridge.h
@@ -1,13 +1,21 @@
-#ifdef WIN32
-#	ifdef ZMQ_BRIDGE_EXPORTS
-#		define ZMQ_BRIDGE_API __declspec(dllexport)
-#	else
-#		define ZMQ_BRIDGE_API __declspec(dllimport)
-#	endif
-#elif defined(__GNUC__) && __GNUC__ >= 4
-#	define ZMQ_BRIDGE_API __attribute__ ((visibility("default")))
-#else
-#	define ZMQ_BRIDGE_API
-#endif
+#include "zmq.h"
+#include "zmqbridge.h"
+#include <mutex>
 
-ZMQ_BRIDGE_API void* zmq_bridge();
+std::mutex m;
+
+// THIS WILL CREATE A CONTEXT THAT CAN BE USED AS A GLOBAL CONTEXT
+// IF NODE.JS IS EMBEDED, HOST BINARY CAN ALSO CALL `zmq_bridge()` 
+// TO OBTAIN SAME CONTEXT, ENABLING inproc:// BETWEEN HOST AND
+// NODE THREADS
+void *_zmq_bridge = NULL;
+
+ZMQ_BRIDGE_API void *zmq_bridge() {
+    std::scoped_lock lock(m);
+
+    if (_zmq_bridge == NULL) {
+        _zmq_bridge = zmq_ctx_new();
+    }
+
+    return _zmq_bridge;
+}
