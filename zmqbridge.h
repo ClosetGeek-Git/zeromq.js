d1c66a252925d1d3b2b7bcb5785373d366ba1c85
JSON <jasonrlester@yahoo.com>
2024-02-27 14:37:15 -0800

    Update zmqbridge.h
    Windows not yet tested

--- zmqbridge.h
+++ zmqbridge.h
@@ -1,1 +1,13 @@
-void* zmq_bridge();
+#ifdef WIN32
+#	ifdef ZMQ_BRIDGE_EXPORTS
+#		define ZMQ_BRIDGE_API __declspec(dllexport)
+#	else
+#		define ZMQ_BRIDGE_API __declspec(dllimport)
+#	endif
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#	define ZMQ_BRIDGE_API __attribute__ ((visibility("default")))
+#else
+#	define ZMQ_BRIDGE_API
+#endif
+
+ZMQ_BRIDGE_API void* zmq_bridge();
